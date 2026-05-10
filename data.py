dfe04327b4a1496d090e0fef73d0e2d2c48b81df
rithsagea <rithsagea@gmail.com>
2026-05-10 00:56:35 -0400

    VFL writeup finished

--- data.py
+++ data.py
@@ -94,6 +94,52 @@ class CaseVFL(Case):
 
         return X, Y
 
+
+@dataclass
+class CaseVFLCorrelated(Case):
+    rng: np.random.Generator
+    n: int
+    events: int
+    output_arity: int
+    parties: int
+    party_arity: int
+
+    noise_std: float
+
+    def __post_init__(self):
+        self.input_events = [
+            LinearEvent(m, b)
+            for m, b in self.rng.uniform(-10, 10, size=(self.events, 2))
+        ]
+        self.output_event = CompositeEvent(
+            [
+                self.input_events[i]
+                for i in self.rng.choice(
+                    len(self.input_events), size=self.output_arity, replace=False
+                )
+            ],
+            self.rng.standard_normal(self.output_arity),
+        )
+
+        self.party_indices = [
+            [(i * self.party_arity + j) % self.events for j in range(self.party_arity)]
+            for i in range(self.parties)
+        ]
+
+    def generate_data(self):
+        data = Data(self.n, self.rng)
+
+        X = [
+            data.get([self.input_events[j] for j in self.party_indices[i]])
+            for i in range(self.parties)
+        ]
+        Y = data.get_single(self.output_event) + self.rng.normal(
+            0, self.noise_std, self.n
+        )
+
+        return X, Y
+
+
 @dataclass
 class CaseHFL(Case):
     rng: np.random.Generator
@@ -108,7 +154,7 @@ class CaseHFL(Case):
             LinearEvent(m, b)
             for m, b in self.rng.uniform(-10, 10, size=(self.events, 2))
         ]
-        
+
         self.output_event = CompositeEvent(
             [
                 self.input_events[i]
@@ -121,9 +167,9 @@ class CaseHFL(Case):
 
     def generate_data(self):
         data = Data(self.n, self.rng)
-        
+
         X_global = data.get(self.input_events)
-        
+
         Y_global = data.get_single(self.output_event) + self.rng.normal(
             0, self.noise_std, self.n
         )
@@ -133,6 +179,7 @@ class CaseHFL(Case):
 
         return X, Y
 
+
 @dataclass
 class CaseHFL_Heterogeneous:
     rng: np.random.Generator
@@ -141,14 +188,14 @@ class CaseHFL_Heterogeneous:
     output_arity: int
     sensors: int
     noise_std: float
-    heterogeneity_std: float = 1.0  
+    heterogeneity_std: float = 1.0
 
     def __post_init__(self):
         self.input_events = [
             LinearEvent(m, b)
             for m, b in self.rng.uniform(-10, 10, size=(self.events, 2))
         ]
-        
+
         active_indices = self.rng.choice(
             len(self.input_events), size=self.output_arity, replace=False
         )
@@ -168,7 +215,7 @@ class CaseHFL_Heterogeneous:
     def generate_data(self):
         X_hetero = []
         Y_hetero = []
-        
+
         if self.n % self.sensors != 0:
             raise ValueError(
                 f"Total samples (n={self.n}) must be perfectly divisible by the "
@@ -177,15 +224,15 @@ class CaseHFL_Heterogeneous:
         num_samples_per_sensor = self.n // self.sensors
 
         for i in range(self.sensors):
-
             local_data = Data(num_samples_per_sensor, self.rng)
             X_local = local_data.get(self.input_events)
-            
-            Y_local = local_data.get_single(self.sensor_output_events[i]) + self.rng.normal(
-                0, self.noise_std, num_samples_per_sensor
-            )
-            
+
+            Y_local = local_data.get_single(
+                self.sensor_output_events[i]
+            ) + self.rng.normal(0, self.noise_std, num_samples_per_sensor)
+
             X_hetero.append(X_local)
             Y_hetero.append(Y_local)
 
         return X_hetero, Y_hetero
+
